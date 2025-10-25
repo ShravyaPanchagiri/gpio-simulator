397ef24c9db83ecbc7c40e5b9deb3988f1bb37ee
ShravyaPanchagiri <shravy112@gmail.com>
2025-10-25 11:37:33 -0500

    Add LED visualization to automated tests for CI

--- tests/gpio_test.c
+++ tests/gpio_test.c
@@ -2,13 +2,37 @@
 #include <assert.h>
 #include <stdio.h>
 
+void display_pins()
+{
+	printf("GPIO Pins: ");
+	for(int i=0;i<8;i++)
+	{
+		if(gpio_get_pin(i) == HIGH)
+			printf("● ");
+		else
+			printf("○ ");
+	}
+	printf("\n");
+}
+
 int main()
 {
 	gpio_init();
+	printf("Test 1: Set pin 0 HIGH\n");
 	gpio_set_pin(0, HIGH);
+	displa_pins();
 	assert(gpio_get_pin(0) == HIGH);
+
+	printf("Test 2: Toggle pin 0 \n");
 	gpio_toggle_pin(0);
+	display_pins();
 	assert(gpio_get_pin(0) == LOW);
+
+	printf("Test 3: Set pin 3 HIGH \n");
+	gpio_set_pin(3, HIGH);
+	display_pins();
+	assert(gpio_get_pin(3) == HIGH);
+	
 	printf("All GPIO tests passed\n");
 	return 0;
 }
